b282b6bd4d389d17853778b0bcc546f6722c7d66
Augusto Soares Porto <115162034+AugustoSoaresPorto@users.noreply.github.com>
2024-11-10 19:07:58 -0300

    final changes

--- tarefa4 - Primeiros programas em assembly para a arquitetura Hack/avg.asm
+++ tarefa4 - Primeiros programas em assembly para a arquitetura Hack/avg.asm
@@ -1,66 +1,82 @@
 // R0 = avg(R1,R2)  Crie um código capaz de calcular a média entre um total de R2 valores armazenados a partir da posição apontada por R1, deixando o resultado em R0. Por exemplo, Se R1 contiver o valor 100 e R2 o valor 10, os valores da memória RAM armazenados entre os endereços 100 a 109 devem ser somados e o resultado divido por 10. Reuse código feito anteriormente.
 
-@I
-M=0            // RI = 0
+@RT1
+M=0
+
+@2
+D=M
+
+@RT2
+M=D-1
+
+@1
+D=M
+
+@RT3
+M=D
+
+@RT2
+D=M
+
+@RT3
+M=D+M
+
+(LOOP)
+@RT3
+A=M
+D=M
+
+@RT1
+M=D+M
+
+@RT3
+M=M-1
+
+@RT2
+M=M-1
+D=M
+
+@LOOP
+D;JGE
+
 @0
-M=0            // R0 = 0
-@SOMA
-0;JMP
-
-(SOMA)
-    @I
-    D=M
-    @2
-    D=D-M
-
-    @AJUSTE
-    D;JEQ
-
-    @I
-    D=M
-    @1
-    D=M+D            // D = R1 + RI
-    @D
-    D=M              // D = M[R1 + RI]
-    @0
-    M=M+D            // R0 = R0 + M[R1 + RI]
-
-    @I
-    M=M+1
-
-    @SOMA
-    0;JMP            // goto SOMA
-
-(AJUSTE)
-    @0
-    D=M              // D = R0
-    @1
-    M=D              // R1 = R0
-    @0
-    M=0              // R0 = 0
-
-    @DIVISAO
-    0;JMP            // goto DIVISAO
+M=0
 
 (DIVISAO)
-    @2
-    D=M              // D = R1
-    @1
-    M=M-D            // R1 = R1 - R2
+@2 
+D=M
 
-    @0
-    M=M+1            // R0 = R0 + 1
+@RT1 
+M=M-D
 
-    @1
-    D=M
-    @2
-    D=D-M
-    @FIM
-    D;JLT            // se D<0 (RESTO R1 < DIVISOR R2) goto FIM
+@0
+M=M+1
 
-    @DIVISAO
-    0;JMP            // goto DIVISAO
+@RT1
+D=M
 
-(FIM)
-    @FIM
-    0;JMP            // goto FIM
+@DIVISAO
+D;JGT
+
+@SAIDA
+D;JEQ
+
+@2
+D=M
+
+@RT1
+M=D+M
+
+@0
+M=M-1
+
+(SAIDA)
+@RT1
+D=M
+
+@RT2
+M=D
+
+@END
+0;JMP
+(END)
