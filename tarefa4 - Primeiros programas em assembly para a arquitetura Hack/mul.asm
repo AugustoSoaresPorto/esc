480e44e0feba6b22386c28c0a1a02231231659dd
Augusto Soares Porto <115162034+AugustoSoaresPorto@users.noreply.github.com>
2024-11-10 19:09:22 -0300

    final changes

--- tarefa4 - Primeiros programas em assembly para a arquitetura Hack/mul.asm
+++ tarefa4 - Primeiros programas em assembly para a arquitetura Hack/mul.asm
@@ -1,24 +1,33 @@
 // R0 = mul(R1,R2)  Crie um código capaz de multiplicar dois números armazenados em R1 e R2  (no fundo, calcular R1*R2), deixando o resultado em R0. Como a ALU não faz multiplicações, trabalhe com a ideia de fazer múltiplas adições na sua solução.
 
+@0
+M=0
+
+@2
+D=M
 @LOOP
+D;JGT
+
+@END
 0;JMP
 
 (LOOP)
-    @1
-    D=M                // D = R1
     @0
-    M=M+D                // R0 = R1
+    D=M
+
+    @1
+    D=D+M
+
+    @0
+    M=D
 
     @2
-    D=M-1              // D = R2
-    M=D                // R2 = R2 - 1
-
-    @FIM
-    D;JEQ              // se D=0 goto FIM
+    D=M-1
+    M=D
 
     @LOOP
-    0;JMP            // goto LOOP
+    D;JGT
 
-(FIM)
-    @FIM
-    0;JMP            // goto FIM
+(END)
+    @END
+    0;JMP
